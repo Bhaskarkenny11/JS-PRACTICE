e96d31e0b9f30578fa4479ed0b5f449bbbc8af14
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-16 11:22:17 +0530

    first commit

---
+++ js2.js
@@ -0,0 +1,21 @@
+var a ="variable using var";
+const b="variable using const"
+let  c = "variable using let";
+console.log(a); // Output: variable using var
+console.log(b)// Output : variable using const
+console.log(c) //using let
+
+var Array=[1,2,3]
+Array.push(4) //adds 4 to last of array
+Array.unshift(0)//adds 0 to the begining of array
+Array.pop()//removes last element
+Array.shift()//removes first element
+let Array1=Array.splice(0,2)
+let Array2=Array1.join()
+console.log(Array1)
+console.log(Array2)
+console.log(Array)
+console.log(typeof(Array2))
+
+
+
