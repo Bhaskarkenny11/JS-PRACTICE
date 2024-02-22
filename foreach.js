322864e3cf17638ee6435f8a30faea4d843dc8db
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-22 12:54:21 +0530

    added

--- foreach.js
+++ foreach.js
@@ -7,5 +7,5 @@ words=sentence.split(" ") // split return array
                     word= word[0].toUpperCase() + word.substr(1,word.length) + " ";
                     output+=word
                 }
-            )
+            );
  console.log(output)
