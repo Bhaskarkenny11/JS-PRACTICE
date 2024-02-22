9ad8ace9c64befda0ccfe5c1648b766756de147c
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-22 13:08:12 +0530

    added comment for anonymous function

--- foreach.js
+++ foreach.js
@@ -2,7 +2,7 @@ var sentence="we all love hyderabad";
 output="#"
 words=sentence.split(" ") // split return array
 // for (var i=0;i<words.length;i++)  this is prebuilt in foreach iterator
-  words.forEach(function(word)
+  words.forEach(function(word) // function with no name its anonymous functio  here function is anonymous
                 {
                     word= word[0].toUpperCase() + word.substr(1,word.length) + " ";
                     output+=word
