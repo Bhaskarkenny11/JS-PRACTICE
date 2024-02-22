24527317783b4c56afe2284d60035b9cb24b95bd
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-22 12:52:07 +0530

    added for each lopp code

---
+++ foreach.js
@@ -0,0 +1,11 @@
+var sentence="we all love hyderabad";
+output="#"
+words=sentence.split(" ") // split return array
+// for (var i=0;i<words.length;i++)  this is prebuilt in foreach iterator
+  words.forEach(function(word)
+                {
+                    word= word[0].toUpperCase() + word.substr(1,word.length) + " ";
+                    output+=word
+                }
+            )
+ console.log(output)
