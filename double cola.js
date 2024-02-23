3e5fe2592005eb9f80764777915266210992a66b
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-23 12:51:02 +0530

    added double cola DSA  probelm

---
+++ double cola.js
@@ -0,0 +1,13 @@
+var queue=["sheldon","leonard","penny","Rajesh","howard"]
+var visited=[]
+var r=6;
+var i=0;
+while(i<r)
+{
+
+ visited=queue.shift();
+ queue.push(visited,visited)
+ i++
+
+}     
+console.log ("person at begining of the queue is " + queue[0])
