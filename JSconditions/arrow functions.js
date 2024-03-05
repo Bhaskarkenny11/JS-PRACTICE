b7ca5c5d2b52809e19a520326cd2e53d0dd84d11
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-05 10:59:24 +0530

    added two more arrow functions

--- JSconditions/arrow functions.js
+++ JSconditions/arrow functions.js
@@ -1,3 +1,26 @@
 
 const evenodd = num => num%2 ? 'odd' : 'even';
 console.log(evenodd(4))
+
+function even (num)
+{
+    if (num%2==0)
+    return "even"
+    else
+    {
+        return "odd"
+    }
+}
+
+console.log(even (5)) 
+
+function evenodd2(num)
+{
+     if (num%2===0)
+      return "even "
+    else 
+        {
+            return "odd"
+        }
+}
+console.log(evenodd2(5));
