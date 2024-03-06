aa38508d550bb5745a421af49d7552d8aa127abb
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-06 12:37:54 +0530

    modified code for having spacing between words

--- strings.js
+++ strings.js
@@ -1,14 +1,15 @@
 var sentence="we all love hyderabad";
 output="#"
 words=sentence.split(" ") // split return array
+//console.log(words)
 for (var i=0;i<words.length;i++)
  {
-    var word=words[i] // # storing array elements in word
-    word[0].toUpperCase() + word.substr(1,words.length) 
-    // we can write like this also 
+   var word=words[i] // # storing array elements in word
+   var  new_word=word[0].toUpperCase() + word.substr(1,word.length)+" "
+   // we can write like this also 
   //var word=words[i][0].toUpperCase() + words[i].substr(1,words[i].length)+" ";
-//    console.log(word);
-   output+=word
+  //console.log(word);
+   output+=new_word
  }
  console.log(output)
 
