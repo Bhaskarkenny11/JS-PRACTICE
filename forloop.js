799a2c407b21d7fe7ce3f5e566b78f99a0dcd92e
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-06 11:56:22 +0530

    tried some new string functions

--- forloop.js
+++ forloop.js
@@ -1,8 +1,8 @@
-var name = "Kenny";
-var lastName="jenny"
+var FirstName = "Kenny";
+var lastName="JENNY"
 var tags="ruby javascript rails ajax python  jquery"
-console.log ("the length of name is " + name.length)
-console.log(name.toUpperCase());
+console.log ("the length of name is " + FirstName.length)
+console.log(FirstName.toUpperCase());
 console.log (lastName.toLowerCase());
 console.log(tags.split(" "));
  var languages=tags.split(" ");
@@ -10,3 +10,13 @@ console.log(tags.split(" "));
  {
     console.log(languages[i].toUpperCase());
  }
+ 
+const fullName= ((FirstName + " "+lastName))
+const id=(FirstName[0] + lastName[0])
+console.log(id)
+console.log(typeof(fullName))
+console.log(fullName.split(" ")[0][0]+fullName.split(" ")[1][0])
+console.log(fullName.trim())
+var str ="   hello  world!  "
+var trimm=console.log(str.trim())
+
