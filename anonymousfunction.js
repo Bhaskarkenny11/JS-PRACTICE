3cdd4f2b3468d2524117704be19cd487cd5a9d3b
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-06 13:19:27 +0530

    added space

--- anonymousfunction.js
+++ anonymousfunction.js
@@ -1,7 +1,19 @@
-var greet =function(name)
-
+var greet=function(name)
+//anonymous function
 {
     return "Hello " + name + "!";
 }
 
 console.log(greet("allen"));
+//-------------------------------------------
+
+var greet2= (name) => "hello"+name 
+
+console.log(greet2("kenny")) ;
+//-------------------------------------------
+function greetname(name)
+{
+    return "hii from standard function "+name
+}
+console.log( greetname("kiran"))
+//--------------------------------------------
