a39fc4eb25f998af81522ab2780667140a30bd03
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-04-02 09:33:55 +0530

    added variables  file

---
+++ variables.js
@@ -0,0 +1,19 @@
+let a=20;
+console.log("a is "+a)
+//let a=24;
+ //throws error  because variable 'a' is declared twice without being initialized again
+
+//Solution:
+{
+    let b = 10;
+    console.log("b is",b);
+}
+
+const c=30;
+//const c=40; //this will throw an error as we are trying
+console.log("const c is" ,c)
+ 
+var d=23
+var d=25
+console.log("var d is",d)
+
