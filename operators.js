e905d9078e7a94623bdacaf7d3e72ed864c74d40
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-04-02 10:05:06 +0530

    added operators file

---
+++ operators.js
@@ -0,0 +1,8 @@
+let a=3;
+let b=4;
+console.log("a+b is",a+b);
+console.log("a+b = " + (a+b));
+console.log("a+b =" + a + b );//gives 34  instead of '7' because it adds the string 'a+' to the number '4'. So, it becomes '34' not '7
+console.log(typeof(a))
+// practice a++, ++a, a--,--a
+//a+=1, a-=1
