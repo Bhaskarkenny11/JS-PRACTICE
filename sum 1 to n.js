4f0f4f98e8e1cb598333a4dfeb111e1c7f89057e
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-04-04 09:40:21 +0530

    added forloop sum 1 to n file

---
+++ sum 1 to n.js
@@ -0,0 +1,16 @@
+let sum=0
+
+for (var i=1;i<=5;i++)
+{
+  (sum=sum+i)
+  console.log("i is =",i)
+   
+}
+console.log("sum is ",sum)
+
+console.log(i)
+
+for (let j=1;j<=5;j++)
+{
+    console.log("hii hello")
+} 
